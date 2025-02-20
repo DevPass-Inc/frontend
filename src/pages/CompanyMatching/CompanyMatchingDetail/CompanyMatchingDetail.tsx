39e0228b71fc6bae9e4f440e12e064122d38cfd1
nagosu <nozick1021@gmail.com>
2025-02-20 16:14:38 +0900

    design: 상세 정보 페이지 퍼블리싱

--- src/pages/CompanyMatching/CompanyMatchingDetail/CompanyMatchingDetail.tsx
+++ src/pages/CompanyMatching/CompanyMatchingDetail/CompanyMatchingDetail.tsx
@@ -1,7 +1,14 @@
+import { useNavigate } from 'react-router-dom';
 import arrowLeftIcon from '/images/svg/icons/arrow_left.svg';
+import CompanyMatchingDetailInfo from '../../../components/CompanyMatchingDetailInfo';
 
 function CompanyMatchingDetail() {
-  const navigtat;
+  const navigate = useNavigate();
+
+  // 매칭 결과로 돌아가기 버튼 클릭 핸들러
+  const handleGoBackToResult = () => {
+    navigate('/company/matching/result');
+  };
 
   return (
     <div className='w-main flex justify-center overflow-hidden'>
@@ -11,12 +18,16 @@ function CompanyMatchingDetail() {
           <button
             type='button'
             className='flex cursor-pointer items-center gap-2.5'
+            onClick={handleGoBackToResult}
           >
             <img src={arrowLeftIcon} alt='Go Back' className='w-6' />
             <span className='leading-[19.36px] font-medium'>
               매칭 결과로 돌아가기
             </span>
           </button>
+
+          {/* 기업 상세 */}
+          <CompanyMatchingDetailInfo />
         </div>
       </div>
     </div>
