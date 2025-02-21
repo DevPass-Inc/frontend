57557ab3a35629ccd54d812c97d1147384a99bef
nagosu <nozick1021@gmail.com>
2025-02-21 17:47:49 +0900

    design: 스텝 프로그레스 바 컴포넌트 구현

---
+++ src/shared/components/Stepper/Stepper.tsx
@@ -0,0 +1,30 @@
+import '../../../lib/stepper/css/styles.css';
+import checkWhiteIcon from '/images/svg/icons/check_white.svg';
+
+interface StepperProps {
+  step: string[];
+  currentStep: number;
+  width: number;
+}
+
+function Stepper(props: StepperProps) {
+  const { step, currentStep, width } = props;
+
+  return (
+    <div className='stepper-wrapper' style={{ width: `${width}px` }}>
+      {step.map((item, idx) => (
+        <div
+          key={`stepper-item-${idx}`}
+          className={`stepper-item ${idx < currentStep - 1 ? 'completed' : ''}`}
+        >
+          <div className='step-counter'>
+            <img src={checkWhiteIcon} alt='check-white' className='w-3.5' />
+          </div>
+          <div className='step-name'>{item}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Stepper;
