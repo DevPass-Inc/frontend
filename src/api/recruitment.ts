0934a77d4baef03f7f03b87dcd5e87c779936886
nagosu <nozick1021@gmail.com>
2025-02-26 00:29:56 +0900

    feat: AI 기업 매칭 조회 API 연동

---
+++ src/api/recruitment.ts
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+interface Stack {
+  stack: string;
+  required: boolean;
+}
+
+export interface CompanyMatchingResultItem {
+  companyName: string;
+  position: string;
+  finalScore: string;
+  stacks: Stack[];
+}
+
+export interface FetchCompanyMatchingResultsResponse {
+  httpStatus: string;
+  code: string;
+  message: string;
+  result: CompanyMatchingResultItem[];
+}
+
+// AI 기업 매칭 조회
+export const fetchCompanyMatchingResults = async () => {
+  try {
+    const response = await axios.post<FetchCompanyMatchingResultsResponse>(
+      `http://localhost:8080/api/recruitments/ai`,
+      {
+        userStacks: ['Java', 'Spring Boot'],
+        userResume: 'Java Spring Boot로 API 서버 개발을 해 본 경험이 있습니다.',
+      }
+    );
+
+    console.log(response.data.result);
+
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    throw new Error('기업 매칭 결과 조회에 실패했습니다.');
+  }
+};
